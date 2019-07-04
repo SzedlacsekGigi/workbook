07eee21705cd903924861c233781c54618405444
Dávid Szabó <drszabodavid@gmail.com>
2019-07-04 11:05:07 +0200

    w3 and stack searches added list implemented in hashmap to store searches logos added to buttons

--- src/main/java/com/workbook/dao/QuestionsDao.java
+++ src/main/java/com/workbook/dao/QuestionsDao.java
@@ -1,11 +1,12 @@
 package com.workbook.dao;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public interface QuestionsDao {
 
-    HashMap<String, String> getAll() throws IOException;
+    HashMap<String, ArrayList<String>> getAll() throws IOException;
     void setup(String filePath, Integer fromLine, Integer toLine);
     String read() throws IOException;
 
