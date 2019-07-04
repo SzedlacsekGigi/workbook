be32a799ac1ae4c40c7bd36f62425fe2d3c55069
SzedlacsekGigi <szedlacsek.lilla@gmail.com>
2019-07-04 11:28:50 +0200

    Main page base

---
+++ src/main/java/com/workbook/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.workbook.controller;
+
+import com.workbook.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/"})
+
+
+public class MainController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        context.setVariable("title", "Codecool Tech-Interview Workbook");
+        engine.process("main.html", context, resp.getWriter());
+    }
+
+}
